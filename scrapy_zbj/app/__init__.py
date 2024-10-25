97cb8a62687aa90f9d00d6e3b8a8ffdafa7e4e8c
Ye-yellow <ehelenusandraes3039@gmail.com>
2024-10-25 10:53:04 +0800

    yolov3_zbj
    文字识别验证码

---
+++ scrapy_zbj/app/__init__.py
@@ -0,0 +1,14 @@
+# !/usr/bin/env python
+# -*-coding:utf-8 -*-
+
+"""
+# File       : __init__.py.py
+# Time       ：2024/8/14 16:55
+# Author     ：yujia
+# version    ：python 3.6
+# Description：
+"""
+from src import captcha
+
+# 初始化时候加载模型
+cap_model = captcha.TextSelectCaptcha()
